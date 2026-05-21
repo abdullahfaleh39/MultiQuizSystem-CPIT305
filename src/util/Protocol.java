3a96fd25f205a1f29a57869d9e1d70a232ebd4be
silver39y <silver135700@gmail.com>
2026-05-21 12:40:27 +0300

    Add complete Protocol class

---
+++ src/util/Protocol.java
@@ -0,0 +1,19 @@
+package util;
+
+public class Protocol {
+    public static final String LOGIN = "LOGIN";
+    public static final String REGISTER = "REGISTER";
+
+    public static final String ADD_QUIZ = "ADD_QUIZ";
+    public static final String ADD_QUESTION = "ADD_QUESTION";
+
+    public static final String LIST_QUIZZES = "LIST_QUIZZES";
+    public static final String LIST_QUESTIONS = "LIST_QUESTIONS";
+
+    public static final String SUBMIT_RESULT = "SUBMIT_RESULT";
+    public static final String VIEW_ALL_RESULTS = "VIEW_ALL_RESULTS";
+    public static final String VIEW_STUDENT_RESULTS = "VIEW_STUDENT_RESULTS";
+    public static final String EXPORT_RESULTS_REPORT = "EXPORT_RESULTS_REPORT";
+
+    public static final String EXIT = "EXIT";
+}
